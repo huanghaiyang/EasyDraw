05803e6657d4618b7c799272111bdb482ce0f80b
huanghaiyang <huanghaiyang@aixuexi.com>
2024-12-15 16:14:40 +0800

    add

--- src/modules/stage/shield.ts
+++ src/modules/stage/shield.ts
@@ -7,24 +7,41 @@ export default class StageShield implements IStageShield {
     width: defaults.state.shield.width,
     height: defaults.state.shield.height
   };
+  // 画布
   private canvas: HTMLCanvasElement;
+  // 遮罩画布用以绘制鼠标样式,工具图标等
   private mCanvas: HTMLCanvasElement;
+  // 当前正在使用的创作工具
   private currentCreator: number;
+  // canvas渲染容器
   private renderEl: HTMLDivElement;
+  // 鼠标位置
   private mousePos: IPoint;
+  // 鼠标样式画布,用以加速绘制
+  private cursorCanvasCache: HTMLCanvasElement;
+  // 画布容器尺寸
+  private canvasRectCache: DOMRect;
 
   constructor() {
     this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
   }
 
-  getSize(): ISize {
-    return this.size;
+  // 销毁鼠标样式画布
+  destroyCursorCanvasCache(): void {
+    this.cursorCanvasCache = null;
   }
 
-  getCanvas(): HTMLCanvasElement {
-    return this.canvas;
+  // 清除鼠标样式画布
+  clearCursorCanvasCache(): void {
+    this.cursorCanvasCache.getContext('2d').clearRect(0, 0, this.cursorCanvasCache.width, this.cursorCanvasCache.height);
   }
 
+  /**
+   * 初始化
+   * 
+   * @param renderEl 
+   */
   async init(renderEl: HTMLDivElement): Promise<void> {
     this.renderEl = renderEl;
     Promise.all([
@@ -33,6 +50,9 @@ export default class StageShield implements IStageShield {
     ])
   }
 
+  /**
+   * 初始化画布
+   */
   async initCanvas(): Promise<void> {
     this.mCanvas = document.createElement('canvas');
     this.mCanvas.id = 'm-shield';
@@ -44,6 +64,9 @@ export default class StageShield implements IStageShield {
     this.renderEl.insertBefore(this.canvas, this.renderEl.firstChild);
   }
 
+  /**
+   * 初始化事件
+   */
   async initEvents(): Promise<void> {
     Promise.all([
       this.initRenderResizeEvent(),
@@ -51,23 +74,67 @@ export default class StageShield implements IStageShield {
     ])
   }
 
+  /**
+   * 初始化鼠标样式画布
+   */
+  async initCanvasCaches(): Promise<void> {
+    this.cursorCanvasCache = document.createElement('canvas');
+    this.cursorCanvasCache.width = 24;
+    this.cursorCanvasCache.height = 24;
+    // 同时绘制图标
+    await this.drawImgLike(this.cursorCanvasCache, this.getCreatorMouseIcon(), {
+      x: 0,
+      y: 0,
+      width: 24,
+      height: 24,
+      color: '#000000'
+    })
+  }
+
+  /**
+   * 初始化画布容器尺寸变更监听
+   */
   async initRenderResizeEvent(): Promise<void> {
     addResizeListener(this.renderEl, () => {
       this.refreshSize();
     })
   }
 
+  /**
+   * 初始化鼠标事件
+   */
   async initMouseEvents(): Promise<void> {
     this.canvas.addEventListener('mousemove', this.handleMouseMove)
+    this.canvas.addEventListener('mouseleave', this.handleMouseLeave)
   }
 
+  /**
+   * 鼠标移动事件
+   * 
+   * @param e 
+   */
   handleMouseMove(e: MouseEvent): void {
     this.calcMousePos(e);
-    this.applyMousePos(e);
+    this.applyMousePosMove(e);
   }
 
+  /**
+   * 鼠标离开画布事件
+   * 
+   * @param e 
+   */
+  handleMouseLeave(e: MouseEvent): void {
+    this.clearMousePos();
+    this.applyMousePosLeave(e)
+  }
+
+  /**
+   * 计算鼠标位置
+   * 
+   * @param e 
+   */
   calcMousePos(e: MouseEvent): IPoint {
-    const { x, y } = this.renderEl.getBoundingClientRect();
+    const { x, y } = this.canvasRectCache;
     this.mousePos = {
       x: e.clientX - x,
       y: e.clientY - y
@@ -75,47 +142,98 @@ export default class StageShield implements IStageShield {
     return this.mousePos;
   }
 
-  applyMousePos(e: MouseEvent): void {
-    this.applyMCanvasMousePos(e)
+  /**
+   * 清除鼠标位置
+   */
+  clearMousePos(): void {
+    this.mousePos = null;
   }
 
-  applyMCanvasMousePos(e: MouseEvent): void {
-    this.renderCreatorIfy(e)
+  /**
+   * 鼠标移动事件
+   * 
+   * @param e 
+   */
+  async applyMousePosMove(e: MouseEvent): Promise<void> {
+    this.applyMCanvasMousePosMove(e)
   }
 
-  renderCreatorIfy(e: MouseEvent): void {
+  /**
+   * 鼠标离开画布事件
+   * 
+   * @param e 
+   */
+  applyMousePosLeave(e: MouseEvent): void {
+    this.clearMCanvas();
+    this.setCanvasCursor('default');
+    this.destroyCursorCanvasCache();
+  }
+
+  /**
+   * 渲染鼠标样式
+   * 
+   * @param e 
+   */
+  async applyMCanvasMousePosMove(e: MouseEvent): Promise<void> {
+    this.renderMCreatorIfy(e)
+  }
+
+  /**
+   * 渲染鼠标样式
+   * 
+   * @param e 
+   */
+  async renderMCreatorIfy(e: MouseEvent): Promise<void> {
+    // 检测当前创作工具是否可用
     if (this.checkCreatorActive(this.currentCreator)) {
+      // 清除画布
       this.clearMCanvas();
-      this.setCanvasCursor('none');
-      this.drawSvg(this.mCanvas, this.getCreatorMouseIcon(), {
-        x: this.mousePos.x - 15,
-        y: this.mousePos.y - 15,
-        width: 30,
-        height: 30,
-        color: '#000'
-      })
+      await this.setCanvasCursor('none');
+      if (this.cursorCanvasCache) {
+        await this.drawImgLike(this.mCanvas, this.cursorCanvasCache, {
+          x: this.mousePos.x - 12,
+          y: this.mousePos.y - 12,
+          width: 24,
+          height: 24,
+          color: '#000000'
+        })
+      } else {
+        await this.initCanvasCaches();
+      }
     } else {
-      this.clearMCanvas();
-      this.setCanvasCursor('default');
+      this.applyMousePosLeave(e);
     }
   }
 
+  /**
+   * 刷新画布尺寸
+   */
   refreshSize(): void {
-    const { width, height } = this.renderEl.getBoundingClientRect();
+    const rect = this.renderEl.getBoundingClientRect();
+    const { width, height } = rect;
     this.size = {
       width: width,
       height: height
     }
+    this.canvasRectCache = rect;
     this.canvas.width = width;
     this.canvas.height = height;
     this.mCanvas.width = width;
     this.mCanvas.height = height;
   }
 
+  /**
+   * 设置当前创作工具
+   * 
+   * @param creator 
+   */
   async setCreator(creator: number): Promise<void> {
     this.currentCreator = creator;
   }
 
+  /**
+   * 获取当前创作工具的鼠标图标
+   */
   getCreatorMouseIcon(): string {
     switch (this.currentCreator) {
       case creatorTypes.shapes:
@@ -125,6 +243,11 @@ export default class StageShield implements IStageShield {
     }
   }
 
+  /**
+   * 检查创作工具是否可用
+   * 
+   * @param creator 
+   */
   checkCreatorActive(creator: number): boolean {
     if ([creatorTypes.shapes].includes(creator)) {
       return true;
@@ -133,11 +256,19 @@ export default class StageShield implements IStageShield {
     }
   }
 
+  /**
+   * 清除遮罩画布内容
+   */
   clearMCanvas(): void {
     this.mCanvas.getContext('2d').clearRect(0, 0, this.mCanvas.width, this.mCanvas.height);
   }
 
-  setCanvasCursor(cursor: string): void {
+  /**
+   * 设置画布鼠标样式
+   * 
+   * @param cursor 
+   */
+  async setCanvasCursor(cursor: string): Promise<void> {
     this.canvas.style.cursor = cursor;
   }
 
@@ -148,13 +279,24 @@ export default class StageShield implements IStageShield {
    * @param svg 
    * @param options 
    */
-  drawSvg(target: HTMLCanvasElement, svg: string, options: { x: number, y: number, width: number, height: number, color: string }): void {
-    const ctx = target.getContext('2d');
-    const img = new Image();
-    img.src = svg;
-    img.onload = () => {
-      ctx.drawImage(img, options.x, options.y, options.width, options.height);
-    }
+  async drawImgLike(target: HTMLCanvasElement, svg: string | HTMLCanvasElement, options: { x: number, y: number, width: number, height: number, color: string }): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const ctx = target.getContext('2d');
+      if (typeof svg === 'string') {
+        const img = new Image();
+        img.src = svg;
+        img.onload = () => {
+          ctx.drawImage(img, options.x, options.y, options.width, options.height);
+          resolve();
+        }
+        img.onerror = () => {
+          reject();
+        }
+      } else if (svg instanceof HTMLCanvasElement) {
+        ctx.drawImage(svg, options.x, options.y, options.width, options.height);
+        resolve();
+      }
+    })
   }
 
 }
