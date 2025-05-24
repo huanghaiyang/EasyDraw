1d2f1a08f242653f8bcb7323dcc507432d0c5e78
huanghaiyang <huanghaiyang@aixuexi.com>
2025-05-24 23:35:32 +0800

    chore: 更新文档中的资源文件路径
    更新了`docs/index.html`中的JavaScript和CSS文件路径，以反映最新的构建输出。

--- docs/assets/lodash-UWhP6zzE.js
+++ docs/assets/lodash-UWhP6zzE.js
@@ -1,4 +1,4 @@
-import{c as Jr}from"./element-B4d41G2h.js";var jt={exports:{}};/**
+import{c as Jr}from"./element-DVUW_5P2.js";var jt={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
